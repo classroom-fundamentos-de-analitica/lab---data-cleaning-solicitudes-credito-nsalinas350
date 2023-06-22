f81076fe610868333f22c2a72f5a8b8e272cb156
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-22 12:13:10 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,19 +13,20 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    # Eliminar filas duplicadas
-    df = df.drop_duplicates()
-
     # Eliminar filas con valores nulos o vacíos
     df = df.dropna()
 
     # Convertir columnas relevantes a tipos de datos adecuados
-    df['sexo'] = df['sexo'].astype(str)
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str)
-    df['idea_negocio'] = df['idea_negocio'].astype(str)
-    df['barrio'] = df['barrio'].astype(str)
+    df['sexo'] = df['sexo'].str.upper()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str).str.upper()
+    df['idea_negocio'] = df['idea_negocio'].astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
+    df['barrio'] = df['barrio'].astype(str).str.lower()
     df['estrato'] = df['estrato'].astype(int)
     df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
     df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'])
+    df['línea_credito'] = df['línea_credito'].str.upper()
+
+    # Eliminar filas duplicadas
+    df = df.drop_duplicates()
 
     return df
