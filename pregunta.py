84455a6ff3b5d497c020ebfb49f998eaa26be44f
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-22 17:31:17 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,27 +7,25 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 
+from datetime import datetime
 import pandas as pd
-
+import re
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
-
-    # Eliminar filas con valores nulos o vacíos
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df.dropna(inplace=True)
-
-    # Convertir columnas relevantes a tipos de datos adecuados
-    df['sexo'] = df['sexo'].str.upper()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str).str.upper()
-    df['idea_negocio'] = df['idea_negocio'].astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
-    df['barrio'] = df['barrio'].str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
-    df['estrato'] = df['estrato'].astype(int)
-    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, errors='coerce')
-    df['monto_del_credito'] = df['monto_del_credito'].replace('[\$,]', '', regex=True).astype(float)
-    df['línea_credito'] = df['línea_credito'].str.replace('-', ' ').str.replace('_', ' ').str.upper()
-
-    # Eliminar filas duplicadas
+    df.fecha_de_beneficio = [datetime.strptime(i, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", i))
+                             else datetime.strptime(i, "%Y/%m/%d")
+                             for i in df.fecha_de_beneficio]
+    df.barrio = [str.lower(i).replace("_", " ").replace("-", " ") for i in df.barrio]
+    df.sexo = df.sexo.str.lower()
+    df.idea_negocio = [str.lower(i.replace("_", " ").replace("-", " ")) for i in df.idea_negocio]
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    df.estrato = df.estrato.astype(int)
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+    df["línea_credito"] = [str.lower(i.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for i in
+                           df["línea_credito"]]
+    df.monto_del_credito = [int(i.replace("$ ", "").replace(".00", "").replace(",", "")) for i in
+                            df.monto_del_credito]
     df.drop_duplicates(inplace=True)
     return df
