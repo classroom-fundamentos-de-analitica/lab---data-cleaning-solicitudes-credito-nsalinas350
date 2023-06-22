7aadf8586f16d5e52f6f8560c0749f1710f1e078
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-22 11:37:54 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,12 +11,22 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    # Cargar los datos
+    df = pd.read_csv('datos.csv')
 
     # Eliminar filas duplicadas
     df = df.drop_duplicates()
 
-    # Eliminar filas con datos faltantes
+    # Eliminar filas con valores nulos o vacíos
     df = df.dropna()
+
+    # Convertir columnas relevantes a tipos de datos adecuados
+    df['sexo'] = df['sexo'].astype(str)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str)
+    df['idea_negocio'] = df['idea_negocio'].astype(str)
+    df['barrio'] = df['barrio'].astype(str)
+    df['estrato'] = df['estrato'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'])
 
     return df
