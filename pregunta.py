c0a75319530484ad8e5f6064ad4758dde3cedffd
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 12:30:27 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -22,7 +22,11 @@ def clean_data():
   df.barrio = df.barrio.str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
   df.estrato = df.estrato.astype(int)
   df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-  df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, format=["%d/%m/%Y", "%Y/%m/%d"], errors="ignore")
+  try:
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, errors='raise')
+  except ValueError:
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], yearfirst=True, errors='coerce')
+
   df.monto_del_credito = df.monto_del_credito.replace('[\$,]', '', regex=True).astype(float)
   df.línea_credito = df.línea_credito.str.replace('-', ' ').str.replace('_', ' ').str.upper()
 
