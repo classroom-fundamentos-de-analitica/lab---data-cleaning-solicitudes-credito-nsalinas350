676c77bdbbe6b8d5e229470b1bd57a460d3dacde
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 11:29:49 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -16,15 +16,15 @@ def clean_data():
   df.dropna(inplace=True)
 
   # Convertir columnas relevantes a tipos de datos adecuados
-  df['sexo'] = df['sexo'].str.upper()
-  df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].astype(str).str.upper()
-  df['idea_negocio'] = df['idea_negocio'].astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
-  df['barrio'] = df['barrio'].str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
-  df['estrato'] = df['estrato'].astype(int)
-  df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
-  df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, errors='coerce')
-  df['monto_del_credito'] = df['monto_del_credito'].replace('[\$,]', '', regex=True).astype(float)
-  df['línea_credito'] = df['línea_credito'].str.replace('-', ' ').str.replace('_', ' ').str.upper()
+  df.sexo = df.sexo.str.upper()
+  df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.astype(str).str.upper()
+  df.idea_negocio = df.idea_negocio.astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
+  df.barrio = df.barrio.str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
+  df.estrato = df.estrato.astype(int)
+  df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+  df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True, errors='coerce')
+  df.monto_del_credito = df.monto_del_credito.replace('[\$,]', '', regex=True).astype(float)
+  df.línea_credito = df.línea_credito.str.replace('-', ' ').str.replace('_', ' ').str.upper()
 
   # Eliminar filas duplicadas
   df.drop_duplicates(inplace=True)
