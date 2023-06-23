2d83567b69734e9ffb787b96daa79374a8ce44f9
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 11:45:27 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,21 +11,20 @@ import pandas as pd
 
 def clean_data():
   df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
-
-  # Eliminar filas con valores nulos o vacíos
   df.dropna(inplace=True)
-
-  # Convertir columnas relevantes a tipos de datos adecuados
+  # df.fecha_de_beneficio = [datetime.strptime(i, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", i))
+  # else datetime.strptime(i, "%Y/%m/%d")
+  # for i in df.fecha_de_beneficio]
+  df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True, errors='coerce')
+  df.barrio = [str.lower(i).replace("_", " ").replace("-", " ") for i in df.barrio]
   df.sexo = df.sexo.str.lower()
-  df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.astype(str).str.lower()
-  df.idea_negocio = df.idea_negocio.astype(str).str.replace('-', ' ').str.replace('_', ' ').str.lower()
-  df.barrio = df.barrio.str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
+  df.idea_negocio = [str.lower(i.replace("_", " ").replace("-", " ")) for i in df.idea_negocio]
+  df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
   df.estrato = df.estrato.astype(int)
   df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-  df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True, errors='coerce')
-  df.monto_del_credito = df.monto_del_credito.replace('[\$,]', '', regex=True).astype(float)
-  df.línea_credito = df.línea_credito.str.replace('-', ' ').str.replace('_', ' ').str.lower()
-
-  # Eliminar filas duplicadas
+  df["línea_credito"] = [str.lower(i.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for i in
+                         df["línea_credito"]]
+  df.monto_del_credito = [int(i.replace("$ ", "").replace(".00", "").replace(",", "")) for i in
+                          df.monto_del_credito]
   df.drop_duplicates(inplace=True)
   return df
