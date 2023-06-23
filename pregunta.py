0d58f0ef112f1366fce9529563031ce2979853b7
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 12:44:53 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,8 +6,8 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-
 import pandas as pd
+
 
 def clean_data():
   df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
@@ -15,15 +15,15 @@ def clean_data():
   # Eliminar filas con valores nulos o vacíos
   df.dropna(inplace=True)
 
-  # Convertir columnas relevantes a tipos de datos adecuados
+  # Convertir columnas
   df.sexo = df.sexo.str.upper()
   df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.astype(str).str.upper()
   df.idea_negocio = df.idea_negocio.astype(str).str.replace('-', ' ').str.replace('_', ' ').str.upper()
   df.barrio = df.barrio.str.replace('-', ' ').str.replace('_', ' ').astype(str).str.lower()
   df.estrato = df.estrato.astype(int)
   df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-  df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True,yearfirst=True, errors='ignore')
-  # df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], yearfirst=True, errors='ignore')
+  df.fecha_de_beneficio = [pd.to_datetime(i, format="%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", i))
+  else pd.to_datetime(i, format="%Y/%m/%d") for i in df['fecha_de_beneficio']]
   df.monto_del_credito = df.monto_del_credito.replace('[\$,]', '', regex=True).astype(float)
   df.línea_credito = df.línea_credito.str.replace('-', ' ').str.replace('_', ' ').str.upper()
 
